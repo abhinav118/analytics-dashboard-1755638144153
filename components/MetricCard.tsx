e1609ee642ce6e5e6a83f5c526d24f8d571e7445
Abhi <abhinav118@gmail.com>
2025-08-19 17:15:51 -0400

    Add components/MetricCard.tsx

---
+++ components/MetricCard.tsx
@@ -0,0 +1,14 @@
+interface MetricCardProps {
+  title?: any
+  value?: any
+  change?: any
+  trend?: any
+}
+
+export default function MetricCard({ title, value, change, trend }: MetricCardProps) {
+  return (
+    <div className="">
+      {/* MetricCard component content */}
+    </div>
+  )
+}
