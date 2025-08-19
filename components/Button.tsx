32d89d5c3690cf0c3e8264d67ab19cfec340f13a
Abhi <abhinav118@gmail.com>
2025-08-19 17:15:53 -0400

    Add components/Button.tsx

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  variant?: any
+  size?: any
+  disabled?: any
+  loading?: any
+}
+
+export default function Button({ variant, size, disabled, loading }: ButtonProps) {
+  return (
+    <div className="">
+      {/* Button component content */}
+    </div>
+  )
+}
