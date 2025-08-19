542033fa5484f534282843218ff0f632e0764aa2
Abhi <abhinav118@gmail.com>
2025-08-19 17:15:52 -0400

    Add components/DataTable.tsx

---
+++ components/DataTable.tsx
@@ -0,0 +1,15 @@
+interface DataTableProps {
+  data?: any
+  columns?: any
+  searchable?: any
+  sortable?: any
+  pagination?: any
+}
+
+export default function DataTable({ data, columns, searchable, sortable, pagination }: DataTableProps) {
+  return (
+    <div className="">
+      {/* DataTable component content */}
+    </div>
+  )
+}
