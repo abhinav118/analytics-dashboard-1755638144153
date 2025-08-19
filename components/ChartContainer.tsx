a3360d369c77977e2054edb62f13e54eb26a4980
Abhi <abhinav118@gmail.com>
2025-08-19 17:15:52 -0400

    Add components/ChartContainer.tsx

---
+++ components/ChartContainer.tsx
@@ -0,0 +1,14 @@
+interface ChartContainerProps {
+  title?: any
+  chartType?: any
+  data?: any
+  options?: any
+}
+
+export default function ChartContainer({ title, chartType, data, options }: ChartContainerProps) {
+  return (
+    <div className="">
+      {/* ChartContainer component content */}
+    </div>
+  )
+}
